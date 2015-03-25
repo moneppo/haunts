083a2df0be0473378ccf7f8f3dbba6f73f6db8bb
Michael Oneppo <moneppo@gmail.com>
2015-03-25 11:40:13 -0400

    Initial Commit

---
+++ Haunts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Haunts
+//
+//  Created by Michael Oneppo on 3/25/15.
+//  Copyright (c) 2015 moneppo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
